441297b09e671cfbbaea2e4bb1f258be34663584
Amina <amina_marie@outlook.fr>
2025-10-20 12:47:44 +0200

    mis à jour

---
+++ app.py
@@ -0,0 +1,10 @@
+# app.py
+def addition(a, b):
+    return a + b
+
+def soustraction(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    print("3 + 2 =", addition(3, 2))
+    print("5 - 2 =", soustraction(5, 2))
---
+++ test_app.py
@@ -0,0 +1,8 @@
+# test_app.py
+from app import addition, soustraction
+
+def test_addition():
+    assert addition(2, 3) == 5
+
+def test_soustraction():
+    assert soustraction(5, 2) == 3
